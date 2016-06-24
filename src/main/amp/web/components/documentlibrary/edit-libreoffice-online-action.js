e2877fd15bca6d435d5d52129837bdaed26e0eea
Seth Yastrov <syastrov@gmail.com>
2016-06-24 11:47:23 +0200

    Latest changes.

--- src/main/amp/web/components/documentlibrary/edit-libreoffice-online-action.js
+++ src/main/amp/web/components/documentlibrary/edit-libreoffice-online-action.js
@@ -1,15 +1,13 @@
 YAHOO.Bubbling.fire("registerAction", {
-  actionName : "onActionEditInLibreOfficeOnline",
-  fn : function (node) {
-    console.log("hurra3");
-
-    var LOOL_GET_TOKEN = Alfresco.constants.PROXY_URI + "libreoffice-online/wopi/token";
+  actionName: "onActionEditInLibreOfficeOnline",
+  fn: function (node) {
+    var LOOL_GET_TOKEN = Alfresco.constants.PROXY_URI + "lool/token";
 
     Alfresco.util.Ajax.jsonGet(
         {
           url: LOOL_GET_TOKEN + "?nodeRef=" + encodeURIComponent(node.nodeRef) + "&action=edit",
           successCallback: {
-            fn: function(response) {
+            fn: function (response) {
               var access_token = response.json.access_token;
               var wopi_src_url = response.json.wopi_src_url;
               console.log(access_token, wopi_src_url);
@@ -17,7 +15,7 @@ YAHOO.Bubbling.fire("registerAction", {
               // Get fileId from nodeRef (just use the uuid part)
               var fileId = Alfresco.util.NodeRef(node.nodeRef).id;
 
-              var wopiFileURL = Alfresco.constants.PROXY_URI + "libreoffice-online/wopi/files/" + fileId;
+              var wopiFileURL = Alfresco.constants.PROXY_URI + "wopi/files/" + fileId;
               var frameSrcURL = wopi_src_url + "WOPISrc=" + encodeURIComponent(wopiFileURL);
 
               (function ($) {
@@ -25,16 +23,21 @@ YAHOO.Bubbling.fire("registerAction", {
                     '<input name="access_token" value="' + encodeURIComponent(access_token) + '" type="hidden"/></form>';
                 var frame = '<iframe id="loleafletframe" name= "loleafletframe" allowfullscreen style="width:100%;height:100%;position:absolute;"/>';
 
-                $('body').append(form);
-                $('body').append(frame);
+                $('#loolcontainer').remove();
 
-                $('#loleafletframe').load(function() {
+                var container = '<div id="loolcontainer" style="position: fixed; height: 100%; width: 100%; top: 0; left: 0; background: white;"></div>';
+                $('body').append(container);
+                var loolContainer = $('#loolcontainer');
+
+                loolContainer.append(form);
+                loolContainer.append(frame);
+
+                $('#loleafletframe').load(function () {
                   console.log("Loaded loleafletframe");
                 });
 
                 $('#loleafletform').submit();
               })(jQuery);
-              // TODO: Load WOPI URL in iframe
             },
             scope: this
           },
