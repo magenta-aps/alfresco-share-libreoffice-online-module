f3b2f922b1119d9f99b39c04d7e92af5330bffac
Seth Yastrov <syastrov@gmail.com>
2016-06-24 14:09:53 +0200

    Fix jQuery undefined error. Add hacks for getting it working with our servers.

--- src/main/amp/web/components/documentlibrary/edit-libreoffice-online-action.js
+++ src/main/amp/web/components/documentlibrary/edit-libreoffice-online-action.js
@@ -15,10 +15,14 @@ YAHOO.Bubbling.fire("registerAction", {
               // Get fileId from nodeRef (just use the uuid part)
               var fileId = Alfresco.util.NodeRef(node.nodeRef).id;
 
-              var wopiFileURL = Alfresco.constants.PROXY_URI + "wopi/files/" + fileId;
+              //var wopiFileURL = Alfresco.constants.PROXY_URI + "wopi/files/" + fileId;
+              // TODO: Use the commented out line above.. the following line is
+              // a hack:
+              var wopiFileURL = "https://alfedu.magenta.dk/alfresco/service/" + "wopi/files/" + fileId;
+
               var frameSrcURL = wopi_src_url + "WOPISrc=" + encodeURIComponent(wopiFileURL);
 
-              (function ($) {
+              require(["jquery"], (function ($) {
                 var form = '<form id="loleafletform" name="loleafletform" target="loleafletframe" action="' + frameSrcURL + '" method="post">' +
                     '<input name="access_token" value="' + encodeURIComponent(access_token) + '" type="hidden"/></form>';
                 var frame = '<iframe id="loleafletframe" name= "loleafletframe" allowfullscreen style="width:100%;height:100%;position:absolute;"/>';
@@ -37,7 +41,7 @@ YAHOO.Bubbling.fire("registerAction", {
                 });
 
                 $('#loleafletform').submit();
-              })(jQuery);
+              }));
             },
             scope: this
           },
